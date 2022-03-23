15503dfb4fb39b945cf72e3fbd22b44b728fbf06
Heo <Heo@DESKTOP-1RQBDFM>
2022-03-23 17:56:33 +0900

    CScene_Title에서 백그라운드 이미지 3개 클래스를 만든 것을 하나로 수정, 시작 그리고 종료버튼 2개 만듬

--- WinApi2D/CScene_Title.cpp
+++ WinApi2D/CScene_Title.cpp
@@ -1,8 +1,7 @@
 #include "framework.h"
 #include "CScene_Title.h"
 #include "CImageObject.h"
-#include "CBackGroundObject.h"
-#include "CBgHousObject.h"
+#include "CImageButton.h"
 
 CScene_Title::CScene_Title()
 {
@@ -18,28 +17,40 @@ void CScene_Title::Enter()
 {
 	// TODO : 
 	// 1. 백그라운드 출력용 오브젝트 제작
-	CBackGroundObject* BackGroundObjcet = new CBackGroundObject;
-	BackGroundObjcet->SetPos(fPoint(0.f, 0.f));
-	BackGroundObjcet->SetScale(fPoint(1280.f, 720.f));
-	BackGroundObjcet->Load(L"BakcGroundImage", L"texture\\title_bg.png");
-	AddObject(BackGroundObjcet, GROUP_GAMEOBJ::BACKGROUND);
+	CImageObject* backGroundObjcet = new CImageObject;
+	backGroundObjcet->SetPos(fPoint(0.f, 0.f));
+	backGroundObjcet->SetScale(fPoint(1280.f, 720.f));
+	backGroundObjcet->Load(L"BakcGroundImage", L"texture\\title_bg.png");
+	AddObject(backGroundObjcet, GROUP_GAMEOBJ::BACKGROUND);
 
 	// 2. 백그라운드 이미지 출력용 오브젝트 제작
-	CBgHousObject* BgHousObjcet = new CBgHousObject;
-	BgHousObjcet->SetPos(fPoint(0.f, 500.f));
-	BgHousObjcet->SetScale(fPoint(1280.f, 220.f));
-	BgHousObjcet->Load(L"BgHousObjcetImage", L"texture\\title_house.png");
-	AddObject(BgHousObjcet, GROUP_GAMEOBJ::BACKGROUND);
+	CImageObject* bgHousObjcet = new CImageObject;
+	bgHousObjcet->SetPos(fPoint(0.f, 500.f));
+	bgHousObjcet->SetScale(fPoint(1280.f, 220.f));
+	bgHousObjcet->Load(L"BgHousObjcetImage", L"texture\\title_house.png");
+	AddObject(bgHousObjcet, GROUP_GAMEOBJ::BACKGROUND);
  
 	// 3. 로고 출력용 오브젝트 제작
 	CImageObject* logoObjcet = new CImageObject;
-	logoObjcet->SetPos(fPoint(250.f, 100.f));
+	logoObjcet->SetPos(fPoint(270.f, 100.f));
 	logoObjcet->SetScale(fPoint(720.f, 200.f));
 	logoObjcet->Load(L"LogoImage", L"texture\\title.png");
 	AddObject(logoObjcet, GROUP_GAMEOBJ::BACKGROUND);
 
 	// 4. 시작 버튼
+	CImageButton* startButton = new CImageButton;
+	startButton->Load(L"TitleButton", L"texture\\title_button.png");
+	startButton->SetText(L" 시작 하기");
+	startButton->SetPos(fPoint(500.f, 600.f));
+	startButton->SetScale(fPoint(300.f, 100.f));
+	AddObject(startButton, GROUP_GAMEOBJ::UI);
+
 	// 5. 종료 버튼
+	CImageButton* exitButton = new CImageButton;
+	exitButton->Load(L"ExitButton", L"texture\\exit_button.png");
+	exitButton->SetPos(fPoint(1170.f,650.f));
+	exitButton->SetScale(fPoint(100.f, 50.f));
+	AddObject(exitButton, GROUP_GAMEOBJ::UI);
 }
 
 void CScene_Title::Exit()
--- WinApi2D/CScene_Title.h
+++ WinApi2D/CScene_Title.h
@@ -6,10 +6,7 @@ public:
 	CScene_Title();
 	virtual ~CScene_Title();
 
-
-
 	virtual void Enter();	// 해당 씬에 진입시 호출
 	virtual void Exit();	// 해당 씬을 탈출시 호출
-
 };
 
